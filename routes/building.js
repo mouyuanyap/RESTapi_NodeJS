2ea461b361cc7a7cb94999953e6ce76444e7f85b
Mou Yuan Yap <mouyuanyap@yahoo.com>
2021-01-20 16:22:14 +0800

    added authentication

---
+++ routes/building.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const building = require('../services/getBuilding');
+const authorize = require('../_middleware/authorize')
+
+router.get('/', authorize(),async function(req, res, next) {
+  try {
+    res.json(await building.getMultiple());
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/:id',authorize(), async function(req, res, next) {
+  try {
+    res.json(await building.getSpecific(req.params.id));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/filterBlock/:id',authorize(), async function(req, res, next) {
+  try {
+    res.json(await building.getMultipleFilterBlock(req.params.id));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/filterFloor/:id', authorize(),async function(req, res, next) {
+  try {
+    res.json(await building.getMultipleFilterFloor(req.params.id));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/filterAll/:block/:floor', authorize(),async function(req, res, next) {
+  try {
+    res.json(await building.getMultipleFilterAll(req.params.block,req.params.floor));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/:id/usage', authorize(),async function(req, res, next) {
+  try {
+    res.json(await building.getUsage(req.params.id));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+router.get('/:id/usageAvg', authorize(),async function(req, res, next) {
+  try {
+    res.json(await building.getUsageAvg(req.params.id));
+  } catch (err) {
+    console.error(`Error while getting quotes `, err.message);
+    next(err);
+  }
+});
+
+
+router.post('/:id', async function(req, res, next) {
+  try {
+    console.log(req.body)
+    res.json(await building.insert(req.body));
+  } catch (err) {
+    console.error(`Error while getting quotes??? `, err.message);
+    next(err);
+  }
+});
+
+module.exports = router;
---
+++ services/getBuilding.js
@@ -0,0 +1,137 @@
+const db = require('./db');
+
+async function getMultiple(){
+  const Buildings = await db.query(`
+  SELECT * FROM c180_property.propertyunits;
+  `);
+
+  return {
+    Buildings
+  }
+}
+
+async function getMultipleFilterBlock(block){
+  const Buildings = await db.query(`
+  SELECT * FROM c180_property.propertyunits WHERE UnitBlock = ?;
+  `,[block]);
+
+  return {
+    Buildings
+  }
+}
+
+async function getMultipleFilterFloor(floor){
+  const Buildings = await db.query(`
+  SELECT * FROM c180_property.propertyunits WHERE UnitFloor = ?;
+  `,[floor]);
+
+  return {
+    Buildings
+  }
+}
+
+async function getMultipleFilterAll(block,floor){
+  console.log(block)
+  console.log(floor)
+  const Buildings = await db.query(`
+  SELECT * FROM c180_property.propertyunits WHERE UnitBlock = ? AND UnitFloor = ?;
+  `,[block,floor]);
+
+  return {
+    Buildings
+  }
+}
+
+
+async function getSpecific(id){
+  const Records = await db.query(`
+  SELECT RecordID, RecordTime, UserName,RecordPropertyID,UnitBlock,UnitFloor,UnitName,RecordReading,RecordStatus 
+  FROM c180_property.watermeterrecord JOIN c180_property.propertyunits JOIN c180_property.users 
+  WHERE watermeterrecord.RecordPropertyID = propertyunits.UnitID AND RecordPropertyID = ?
+  AND users.UserID = watermeterrecord.RecordUserID
+  ORDER BY RecordTime DESC;
+  `,[id]);
+
+  return {
+    Records
+  }
+}
+
+async function getUsage(id){
+  const Usage = await db.query(`
+  SELECT * FROM c180_property.waterusage WHERE UsagePropertyID = ?
+  ORDER BY PeriodEnd DESC LIMIT 1;
+  `,[id]);
+
+  return {
+    Usage
+  }
+}
+
+async function getUsageAvg(id){
+  const UsageAvg = await db.query(`
+  SELECT usagePropertyID, AVG(UsageData) as AverageUsage FROM c180_property.waterusage WHERE usagePropertyID = ? GROUP BY usagePropertyID;;
+  `,[id]);
+
+  return {
+    UsageAvg
+  }
+}
+
+
+
+
+function validateCreate(quote) {
+  let messages = [];
+
+  console.log(quote);
+
+  if (!quote) {
+    messages.push('No object is provided');
+  }
+
+  if (!quote.SubmitReading) {
+    messages.push('Quote is empty');
+  }
+
+  if (!quote.SubmitRecordID) {
+    messages.push('Quote is empty');
+  }
+
+  
+  if (messages.length) {
+    let error = new Error(messages.join());
+    error.statusCode = 400;
+
+    throw error;
+  }
+}
+
+async function insert(quote){
+  validateCreate(quote);
+
+  const result = await db.query(
+    'INSERT INTO WaterMeterRecord(RecordUserID,RecordPropertyID,RecordReading,RecordStatus) Values(001,?,?,"Pending");;', 
+    [quote.SubmitRecordID, quote.SubmitReading]
+  );
+
+  let message = 'Error in creating quote';
+
+  if (result.affectedRows) {
+    message = 'Quote created successfully';
+  }
+
+  return {message};
+}
+
+
+module.exports = {
+  getMultiple,
+  getSpecific,
+  insert,
+  getUsage,
+  getUsageAvg,
+  getMultipleFilterBlock,
+  getMultipleFilterFloor,
+  getMultipleFilterAll
+}
